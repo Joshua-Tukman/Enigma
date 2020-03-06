d051915a65dab060d43cb629d9bb6a4db1cadbc1
Josh Tukman <josh.tukman@gmail.com>
2020-03-06 14:47:46 -0700

    Add edge case tests for handling  non alphabet characters through out string

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -12,14 +12,42 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_it_can_encrypt_a_message_with_a_key_and_date
-
     enigma = Enigma.new
     expected = {
-      encryption: "k*eder ohulw!",
+      encryption: "keder ohulw",
       key: "02715",
       date: "040895"
     }
-    assert_equal expected, enigma.encrypt("h*ello world!", "02715", "040895")
+    assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
+  end
+
+  def test_it_can_encrypt_with_non_alphabet_characters_at_beginning
+    enigma = Enigma.new
+    expected = {
+      encryption: "$keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+    assert_equal expected, enigma.encrypt("$hello world", "02715", "040895")
+  end
+  def test_it_can_encrypt_with_non_alphabet_characters_in_middle
+    enigma = Enigma.new
+    expected = {
+      encryption: "keder, ohulw",
+      key: "02715",
+      date: "040895"
+    }
+    assert_equal expected, enigma.encrypt("hello, world", "02715", "040895")
+  end
+
+  def test_it_can_encrypt_with_non_alphabet_characters_at_end
+    enigma = Enigma.new
+    expected = {
+      encryption: "keder ohulw!",
+      key: "02715",
+      date: "040895"
+    }
+    assert_equal expected, enigma.encrypt("hello world!", "02715", "040895")
   end
 
   def test_it_can_decrypt_message_with_a_key_and_date
@@ -32,16 +60,15 @@ class EnigmaTest < Minitest::Test
     assert_equal expected, enigma.decrypt("keder ohulw!", "02715", "040895")
   end
 
-  # def test_it_can_encrypt_a_message_with_todays_date
-  #   enigma = Enigma.new
-  #   expected = {
-  #     encryption: " " ,
-  #     key: "02715",
-  #     date: "060320"
-  #   }
-  #   assert_equal expected, enigma.encrypt("helloworld", "02715")
-  #
-  # end
+  def test_it_can_encrypt_a_message_with_todays_date
+    enigma = Enigma.new
+    expected = {
+      encryption: " " ,
+      key: "02715",
+      date: "060320"
+    }
+    assert_equal expected, enigma.encrypt("hello world", "02715")
+  end
 end
 
 # # encrypt a message with a key (uses today's date)
